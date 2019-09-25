9571bdbb1381788f46841471d5a0c9dd8c197107
Chandu <Chandra.Vedantham@Gmail.com>
2019-09-25 23:34:41 +0530

    Adds Linting/Formatting for Typescript files.

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,15 @@
-import * as React from "react";
+import * as React from 'react'
 
-import './App.scss';
+import './App.scss'
 
-export interface AppProps { compiler: string; framework: string; }
-
-// 'HelloProps' describes the shape of props.
-// State is never set so we use the '{}' type.
-export class App extends React.Component<AppProps, {}> {
-  render() {
+export class App extends React.Component {
+  render () {
     return (
       <div>
         <div className="jumbotron">
           <h1>Start coding...</h1>
         </div>
       </div>
-    );
+    )
   }
 }
-
